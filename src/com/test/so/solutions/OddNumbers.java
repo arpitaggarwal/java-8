1be447732372fb30edcf3810f3d8962834752f99
Arpit Aggarwal <ArpitAggarwal@apples-MacBook-Pro.local>
2015-10-17 14:49:47 +0530

    Odd Number - StackOverflow Question

---
+++ src/com/test/so/solutions/OddNumbers.java
@@ -0,0 +1,15 @@
+package com.test.so.solutions;
+
+import java.util.stream.IntStream;
+
+public class OddNumbers {
+
+	public static java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.print("Enter Number : ");
+		int n = scanner.nextInt();
+		IntStream.range(0, n).filter(element -> element % 2 != 0).boxed()
+				.forEach(System.out::println);
+	}
+}
