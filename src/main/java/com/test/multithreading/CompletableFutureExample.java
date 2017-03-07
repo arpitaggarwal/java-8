0cca26f22fbee7d3345d9f7f8b3f975c06efad30
Arpit Aggarwal <aggarwalarpit.89@gmail.com>
2017-03-07 22:41:06 +0400

    CompletableFuture example

---
+++ src/main/java/com/test/multithreading/CompletableFutureExample.java
@@ -0,0 +1,35 @@
+package com.test.multithreading;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureExample {
+	/**
+	 * https://javasampleapproach.com/java/java-8/java-8-completablefuture-
+	 * handle-exception
+	 * 
+	 * @param args
+	 * @throws InterruptedException
+	 * @throws ExecutionException
+	 */
+	public static void main(String[] args) throws InterruptedException,
+			ExecutionException {
+		CompletableFutureExample obj = new CompletableFutureExample();
+		obj.createCompletableFuture();
+	}
+
+	public void createCompletableFuture() throws InterruptedException,
+			ExecutionException {
+		CompletableFuture.runAsync(new Task()).get();
+	}
+}
+
+class Task implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("Hello Task");
+
+	}
+
+}
